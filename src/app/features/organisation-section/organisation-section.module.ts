9e21072ad52a2bd77f04f4234d74ca60dc40acdf
AjitJadhav8 <ajitjadhav30april@gmail.com>
2025-02-11 15:25:29 +0530

    remove classfrom old compoentn

--- src/app/features/organisation-section/organisation-section.module.ts
+++ src/app/features/organisation-section/organisation-section.module.ts
@@ -1,23 +1,48 @@
 import { createComponent, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrganisationDashboardComponent } from './components/organisation-dashboard/organisation-dashboard.component';
-import { CreateComponent } from './components/create/create.component';
-import { ViewComponent } from './components/view/view.component';
-import { DeleteComponent } from './components/delete/delete.component';
+
 import { RouterModule, Routes } from '@angular/router';
 
 
+// import { CreateComponent } from './components/create/create.component';
+// import { ViewComponent } from './components/view/view.component';
+// import { DeleteComponent } from './components/delete/delete.component';
+// const routes: Routes = [
+//   { 
+//     path: '', component: OrganisationDashboardComponent, 
+//     children: [
+//       { path: 'create', component: CreateComponent },
+//       { path: 'view', component: ViewComponent },
+//       { path: 'delete', component: DeleteComponent },
+//     ]
+//   }
+// ];
+
+
+import { CustomerComponent } from './components/customer/customer.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { ProjectComponent } from './components/project/project.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 const routes: Routes = [
   { 
-    path: '', component: OrganisationDashboardComponent, 
+    path: '', component: OrganisationDashboardComponent,
     children: [
-      { path: 'create', component: CreateComponent },
-      { path: 'view', component: ViewComponent },
-      { path: 'delete', component: DeleteComponent },
+      { path: 'customer', component: CustomerComponent },
+      { path: 'employee', component: EmployeeComponent },
+      { path: 'project', component: ProjectComponent },
+      { path: 'admin', component: AdminComponent },
     ]
   }
 ];
+
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -26,10 +51,13 @@ const routes: Routes = [
     CommonModule,
     RouterModule.forChild(routes),
     OrganisationDashboardComponent,
-    CreateComponent,
-    ViewComponent,
-    DeleteComponent,
-
+    // CreateComponent,
+    // ViewComponent,
+    // DeleteComponent,
+    CustomerComponent,
+    EmployeeComponent,
+    ProjectComponent,
+    AdminComponent
   ]
 })
 export class OrganisationSectionModule { }
