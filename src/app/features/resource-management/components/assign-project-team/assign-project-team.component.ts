e012155c0d3897d5c224981d756b6322f8222206
AjitJadhav8 <ajitjadhav30april@gmail.com>
2025-03-28 10:57:03 +0530

    ng select filter ssign team

--- src/app/features/resource-management/components/assign-project-team/assign-project-team.component.ts
+++ src/app/features/resource-management/components/assign-project-team/assign-project-team.component.ts
@@ -518,7 +518,7 @@ export class AssignProjectTeamComponent {
   customerNameFilter: string = '';
   projectNameFilter: string = '';
   employeeNameFilter: string = '';
-  allocationStatusFilter: string = '';
+  allocationStatusFilter: number | null = null;
   billedStatusFilter: number | null = null;
 
   // New filter properties
@@ -549,8 +549,7 @@ export class AssignProjectTeamComponent {
         (this.projectManagerFilter ? team.project_manager_id === this.projectManagerFilter : true) &&
         (this.startDateFilter ? this.formatDate(team.start_date) == this.startDateFilter : true) &&
         (this.endDateFilter ? this.formatDate(team.end_date) == this.endDateFilter : true) &&
-        (this.allocationStatusFilter ? team.allocation_status === this.allocationStatusFilter : true) &&
-        (this.allocationPercentageFilter !== null ? team.allocation_percentage === this.allocationPercentageFilter : true) &&
+        (this.allocationStatusFilter !== null ? team.allocation_status === this.allocationStatusFilter : true) &&        (this.allocationPercentageFilter !== null ? team.allocation_percentage === this.allocationPercentageFilter : true) &&
         (this.billedStatusFilter !== null ? team.billed_status === this.billedStatusFilter : true)&&        (this.billingPercentageFilter !== null ? team.billing_percentage === this.billingPercentageFilter : true)
       );
     });
@@ -574,7 +573,7 @@ export class AssignProjectTeamComponent {
     this.projectManagerFilter = '';
     this.startDateFilter = '';
     this.endDateFilter = '';
-    this.allocationStatusFilter = '';
+    this.allocationStatusFilter = null;
     this.allocationPercentageFilter = null;
     this.billedStatusFilter = null;
     this.billingPercentageFilter = null;
